1cd3bca9d44c05b78ee3459c60e2900f570640c3
Obtains <97128583+Obtains@users.noreply.github.com>
2022-06-29 09:56:59 -0400

    added functionality to thought.js in models

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+// Dependencies
+const { Schema, model, Types } =('mongoose');
+const moment = require('moment');
+const reactionSchema = require('./Reaction');
+
+const thoughtSchema = new Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 280
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => moment(createdAtVal)
+        .format('MMM DD, YYYY [at] hh:mm a')
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+},
+{
+    toJson: {
+        virtuals: true,
+        getters: true
+    },
+    id: false
+});
+
+// Thought model using Thought Schema
+const Thought = model('Thought', thoughtSchema);
+// Total count of Reactions
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
+// Export
+moduke.exports = Thought;
